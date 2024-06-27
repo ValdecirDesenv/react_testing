5925d31d0fb9c79e1089443b5c36d9a7fb7b3ea0
Valdecir <valdecir@Eliss-MacBook-Pro.local>
2024-06-27 00:23:39 -0400

    prettier config

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
-  const [name, setName] = React.useState('bob ')
+  const [name, setName] = React.useState("bob ");
   return (
     <form>
       <div className="mb-3">
-        <label htmlFor="" className="form-label"></label>
-        <input type="text" className="form control" />
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form control" />
       </div>
     </form>
-  )
+  );
 }
 
-export default Form
+export default Form;
